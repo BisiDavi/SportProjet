d23c69c859825bc0cb2b4277870e50fce3a723fe
BisiDavi <oludavidconnect@gmail.com>
2020-09-15 11:15:24 +0100

    .

--- app/Http/Controllers/EnrollController.php
+++ app/Http/Controllers/EnrollController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Newsletter;
 use Illuminate\Http\Request;
 
 class EnrollController extends Controller
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\FooterContactform;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
