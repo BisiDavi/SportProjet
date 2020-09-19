a1ed5f586262d3b53979000822c63d380d75d41a
BisiDavi <oludavidconnect@gmail.com>
2020-09-19 14:45:57 +0100

    .

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -5,16 +5,18 @@
 @endsection
 
 @section('link')
+  <script src="assets/js/accordion.js"></script>
   <link rel="stylesheet" href="{{ asset('assets/css/services.css') }}">
+  <link rel="stylesheet" href="assets/css/aboutaccordion.css">
+  <link rel="stylesheet" href="{{ asset('assets/css/about.css') }}">
 @endsection
-
 
 @section('content')
   <section class="services mt-0">
     <marquee class="marquee-text" direction="right">We offer all round consultancy services in sports and sports related
       matters.
     </marquee>
-    <div class="container-fluid mt-3">
+    <div class="container-fluid large-screen mt-3">
       <div class="row">
         <div class="service-1 col-md-4 col-sm-3 d-flex d-sm-none d-md-block align-items-stretch">
           <div class="card">
@@ -124,6 +126,147 @@
             <div class="card-body">
               <h5 class="card-title"><a href="">BOOT CAMPS/LIFESTYLE</a></h5>
               <ul class="card-text">
+                <li>We believe in the power of influence, networks, and collaborations; hence, we
+                  organize boot camps cities to create a sweet-sweet environment for athletes to
+                  be influenced positively, network with big stars in the industry and collaborate
+                  effectively to grow.</li>
+                <li>Our Lifestyle project is exceptional and plays a key role
+                  in the development
+                  process. Celebrities, stars, big wigs in sports administration within the
+                  country and beyond are all part this initiative.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid mobile-screen mt-3">
+      <div class="row accordion">
+        <div class="col-xs-12 card">
+          <div class="card-img">
+            <img src="assets/img/malefitness.jpg" width="100%" alt="...">
+          </div>
+          <div class="card-body">
+            <h2 class="accordion__items">HEALTH, WELLNESS AND FITNESS CAMPAIGN</h2>
+            <div class="accordion__content">
+              <h3 class="accordion__content__caption">
+                We employ strategies that improves quality of life
+              </h3>
+              <ul class="accordion__content__txt">
+                <li>Dish out strategies that can help improve quality of life for people of various
+                  ages and abilities.</li>
+                <li>Bring light to topics such as athletic injury management,
+                  sport and exercise
+                  psychology, anatomy, biomechanics, wellness management strategies and many more.
+                </li>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-12 card">
+          <div class="card-img">
+            <img src="assets/img/femalefitness.jpg" width="100%" alt="...">
+          </div>
+          <div class="card-body">
+            <h2 class="accordion__items">ACADEMY / TRAINING</h2>
+            <div class="accordion__content">
+              <h3 class="accordion__content__caption">
+                Get trained by Professional to attain Zenith.
+              </h3>
+              <ul class="accordion__content__txt">
+                <li>
+                  We have set up a professional and sustainable training platform where sportsperson of all ages and
+                  levels will have the chance to attain their full potential under the tutelage of passionate, disciplined
+                  and certified training crew.
+                </li>
+                <li>
+                  Our clients/athletes acquire individually streamed training programs and sessions that are designed
+                  uniquely to enhance their talents, while developing the skills and techniques of the athlete, group or
+                  team across a wide range of sporting disciplines.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-12 card">
+          <div class="card-img">
+            <img src="assets/img/football.jpg" width="100%" alt="...">
+          </div>
+          <div class="card-body">
+            <h2 class=" accordion__items">SPORT FESTIVALS / TALENT HUNT SHOWS</h2>
+            <div class="accordion__content">
+              <h3 class="accordion__content__caption">Crucial Topics like Athletic injury management,
+                Sport and exercise
+                psychology, Anatomy, Biomechanics, Wellness management strategies to be learnt
+              </h3>
+              <ul class="accordion__content__txt">
+                <li>Several unique sport festivals are to be organized for various sports, age, sex,
+                  regions of the country. These festivals come with our own unique concept and
+                  programs.
+                </li>
+                <li>We have a sustainable template that we follow which ensures
+                  ha the ultimate goal
+                  of the festival is achieved. We partner with lots of organizations, firms, and
+                  individuals etc. to ensure that each festival is a huge success
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row accordion">
+        <div class="col-xs-12 card">
+          <div class="card-img">
+            <img src="assets/img/banner2.jpg" width="100%" alt="...">
+          </div>
+          <div class="card-body">
+            <h2 class="accordion__items">ATHLETE MANAGEMENT</h2>
+            <div class="accordion__content">
+              <ul class="accordion__content__txt">
+                <li>We have set up a professional and sustainable training platform where
+                  sportsperson of all ages and levels will have the chance to attain their full
+                  potential under the tutelage of passionate, disciplined and certified training
+                  crew.</li>
+                <li>Our clients/athletes acquire individually streamed training
+                  programs and
+                  sessions that are designed uniquely to enhance their talents, while developing
+                  the skills and techniques of the athlete, group or team across a wide range of
+                  sporting disciplines.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-12 card">
+          <div class="card-img">
+            <img src="assets/img/banner2.jpg" width="100%" alt="...">
+          </div>
+          <div class="card-body">
+            <h2 class="accordion__items">SALES AND SUPPLY OF SPORTS MATERIALS</h2>
+            <div class="accordion__content">
+              <ul class="accordion__content__txt">
+                <li>We cover the whole ground in supply and sales of all types of sports materials.
+                  We place premium on quality over quantity, we go for the best and that sets us
+                  apart. We deliver to our clients even in bulk quantities.</li>
+                <li>We of course, partner with the best brands in the world to
+                  deliver on quality
+                  sport materials.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-12 card">
+          <div class="card-img">
+            <img src="assets/img/banner2.jpg" width="100%" alt="...">
+          </div>
+          <div class="card-body">
+            <h2 class="accordion__items">BOOT CAMPS/LIFESTYLE</h2>
+            <div class="accordion__content">
+              <ul class="accordion__content__txt">
                 <li>We believe in the power of influence, networks, and collaborations; hence, we
                   organize boot camps cities to create a sweet-sweet environment for athletes to
                   be influenced positively, network with big stars in the industry and collaborate
